84a93df071a58849348d3665eac67b00d526cfab
hmqgg <mitchx7@outlook.com>
2022-04-18 19:59:39 +0800

    feat(pack): add action-cards

---
+++ setup.js
@@ -0,0 +1,33 @@
+Hooks.on('init', () => {
+  CONFIG.SWADE.init.defaultCardCompendium = 'swade_compendium_chn.action-cards';
+});
+
+Hooks.once('ready', () => {
+  CONFIG.SWADE.vehicles.opSkills = ['', '驾船', '驾驶', '航空', '骑乘'];
+  game.settings.register('swade', 'parryBaseSkill', {
+    name: game.i18n.localize('SWADE.ParryBase'),
+    hint: game.i18n.localize('SWADE.ParryBaseDesc'),
+    default: '格斗',
+    scope: 'world',
+    type: String,
+    config: false,
+  });
+
+  game.settings.register('swade', 'currencyName', {
+    name: game.i18n.localize('SWADE.CurrencyName'),
+    hint: game.i18n.localize('SWADE.CurrencyNameDesc'),
+    scope: 'world',
+    type: String,
+    default: '现金',
+    config: false,
+  });
+
+  game.settings.register('swade', 'coreSkills', {
+    name: game.i18n.localize('SWADE.CoreSkills'),
+    hint: game.i18n.localize('SWADE.CoreSkillsDesc'),
+    default: '运动, 通用知识, 察觉, 交涉, 潜行',
+    scope: 'world',
+    type: String,
+    config: false,
+  });
+});
