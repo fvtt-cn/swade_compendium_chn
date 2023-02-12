d9382444794b3e48d0fbf0e93bb134f98325db48
bnp800 <bnp800onsteam@gmail.com>
2023-02-12 23:09:25 +0800

    add embeddedAbilities surpport

--- babele.js
+++ babele.js
@@ -1,3 +1,19 @@
+function parseEmbeddedAbilities(value, translations, data, tc) {
+	value.forEach( (item, k) => {
+		let pack = game.babele.packs.find(pack => pack.translated && pack.translations[item[1].name]);
+		if(pack && pack !== tc) {
+      value[k][1] = mergeObject(value[k][1], pack.translate(item[1], pack.translations[item[1].name]));
+		}
+	});
+	return value;
+}
+
+Babele.get().registerConverters({
+  "translateEmbeddedAbilities": (value, translations, data, tc) => {
+    return parseEmbeddedAbilities(value, translations, data, tc)
+  }
+});
+
 Hooks.on('init', () => {
   if (typeof Babele !== 'undefined') {
     Babele.get().register({
@@ -6,11 +22,4 @@ Hooks.on('init', () => {
       dir: 'compendium'
     });
   }
-});
-Babele.get().registerConverters({
-  "fromPack": Converters.fromPack({
-    "description": "data.description",
-    "notes": "data.notes",
-    "actions": "data.actions"
-  })
 });
