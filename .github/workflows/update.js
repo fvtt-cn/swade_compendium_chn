33bec5e4fa1ed0561cc68318841cf34693dab0e0
bnp800 <bnp800onsteam@gmail.com>
2023-12-09 16:36:06 +0800

    refactor: mv cn files to zh_Hans

--- .github/workflows/update.js
+++ .github/workflows/update.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 
 const components = fs.readdirSync('./en-US/');
 for (let component of components)
-    if (!fs.existsSync(`./zh-CN/${component}`))
-        fs.writeFileSync(`./zh-CN/${component}`, '{}');
+    if (!fs.existsSync(`./zh_Hans/${component}`))
+        fs.writeFileSync(`./zh_Hans/${component}`, '{}');
