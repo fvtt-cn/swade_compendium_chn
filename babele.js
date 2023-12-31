ab95a30f714823cae36fbb0b071115e4629df688
Leo chen <1515038222@qq.com>
2023-12-31 19:34:24 +0800

    优化了requirements的配置

--- babele.js
+++ babele.js
@@ -1,12 +1,24 @@
 function parseEmbeddedAbilities(value, translations, data, tc) {
-	value.forEach( (item, k) => {
-		let pack = game.babele.packs.find(pack => pack.translated && pack.translations[item[1].name]);
-		if(pack && pack !== tc) {
-      value[k][1] = mergeObject(value[k][1], pack.translate(item[1], pack.translations[item[1].name]));
-		}
-	});
-	return value;
+  value.forEach((item, k) => {
+    let pack = game.babele.packs.find(
+      (pack) => pack.translated && pack.translations[item[1].name]
+    );
+    if (pack && pack !== tc) {
+      value[k][1] = mergeObject(
+        value[k][1],
+        pack.translate(item[1], pack.translations[item[1].name])
+      );
+    }
+  });
+  return value;
 }
+
+Babele.get().registerConverters({
+  translateEmbeddedAbilities: (value, translations, data, tc) => {
+    return parseEmbeddedAbilities(value, translations, data, tc);
+  },
+});
+
 const SWADE_ITEM_MAPPING = {
   description: "system.description",
   notes: "system.notes",
@@ -16,21 +28,30 @@ const SWADE_ITEM_MAPPING = {
   category: "system.category",
 };
 const SWADE_ITEM_CONVERTERS = Converters.fromPack(SWADE_ITEM_MAPPING, "Item");
+function SWADE_REQUIREMENTS_CONVERTER(value, translations) {
+  const result = [];
+  for (let raw of value) {
+    const rawToR = {};
+    Object.assign(rawToR, raw);
+    if (rawToR.type=='rank')
+    result.push(rawToR);
+  }
+  const other={type:'other',label:translations}
+  result.push(other)
+  return result;
+}
 Babele.get().registerConverters({
   SWADE_ITEM_CONVERTERS: SWADE_ITEM_CONVERTERS,
+  SWADE_REQUIREMENTS_CONVERTER: SWADE_REQUIREMENTS_CONVERTER,
 });
-Babele.get().registerConverters({
-  "translateEmbeddedAbilities": (value, translations, data, tc) => {
-    return parseEmbeddedAbilities(value, translations, data, tc)
-  }
-});
-
-Hooks.on('init', () => {
-  if (typeof Babele !== 'undefined') {
+Hooks.on("init", () => {
+  if (typeof Babele !== "undefined") {
+    console.log("mapping", Babele);
+    console.log(Converters);
     Babele.get().register({
-      module: 'swade_compendium_chn',
-      lang: 'cn',
-      dir: 'compendium'
+      module: "swade_compendium_chn",
+      lang: "cn",
+      dir: "compendium",
     });
   }
 });
